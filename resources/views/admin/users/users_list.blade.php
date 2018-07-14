cfd304fc219eea9aad583890c7546d99503a7ac7
naciriii <nnacir1938@hotmail.com>
2018-07-14 02:53:22 +0200

    fix

--- resources/views/admin/users/users_list.blade.php
+++ resources/views/admin/users/users_list.blade.php
@@ -44,12 +44,13 @@
               <tr>
                 <td>{{$row->name}}</td>
                 <td>{{$row->email}}</td>
-                <td>{{$row->detail->email or ''}}</td>
-                <td>{{$row->detail->identifier or ''}}</td>
-             <td>{{$row->detail->first_name or ''}}</td>
-             <td>{{$row->detail->last_name or ''}}</td>
-               <td>{{$row->detail->first_name or ''}}</td>
-             <td>{{$row->detail->birthday or ''}}</td>
+                <td>{{$row->details->email or ''}}</td>
+                <td>{{$row->details->identifier or ''}}</td>
+             <td>{{$row->details->first_name or ''}}</td>
+             <td>{{$row->details->last_name or ''}}</td>
+               <td>@if($row->details && $row->details->picture != null)
+               <img src="{{$row->details->picture}}" height="50" width="50"> @endif</td>
+             <td>{{$row->details->birthday or ''}}</td>
              <td>{{str_limit($row->created_at,16,'')}}</td>
 
 
